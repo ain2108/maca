62d35306ef33b7a15407d6dabfd8749f82fec516
Anton Nefedenkov <ain2108@columbia.edu>
2022-08-10 14:27:43 -0400

    added simple version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import { useState } from 'react'
+
+import { Button, IconButton } from '@mui/material';
+
+enum Category {
+  Games = "Games",
+  Books = "Books",
+  Films = "Films",
+  Paintings = "Paintings",
+  Links = "Links",
+}
+
+
+
+async function getResources(category: Category): Promise<Resource[]> {
+  try {
+    const response = await fetch('http://localhost:4000/api/resources/' + category)
+    const json = await response.json();
+    console.log(json)
+    return json
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+type Resource = {
+  title: string;
+  category: Category;
+}
+
+function MyButton({ setResources, category }: { setResources: React.Dispatch<React.SetStateAction<Resource[]>>, category: Category }) {
+
+  function handleClick(): void {
+    getResources(category)
+      .then(rs => setResources(rs))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Button onClick={handleClick} variant="text">{category}</Button>
+
+  );
+}
+
+function ResourceElement({ resource }: { resource: Resource }) {
+  return (
+    <tr>
+      <td> {resource.title}</td>
+    </tr>
+  );
+}
+
+function ResourceTable({ resources }: { resources: Resource[] }) {
+
+  let rows = resources.map(r =>
+    <ResourceElement resource={r} />
+  )
+
+  return (
+    <table>
+      <thead>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  )
+}
+
+function Buttons({ categories, setResources }: { categories: Category[], setResources: React.Dispatch<React.SetStateAction<Resource[]>> }) {
+  const buttons = categories.map(c => <MyButton category={c} setResources={setResources} />)
+  return <div>{buttons}</div>
+}
+
+export default function MyApp() {
+
+
+  var initEmptyResource: Resource[] = []
+  const [resources, setResources] = useState(initEmptyResource)
+
+  return (
+    <div>
+      <h1>Welcome to Maca Bay</h1>
+      <Buttons categories={Object.values(Category)} setResources={setResources} />
+      <ResourceTable resources={resources} />
+
     </div>
   );
 }
 
-export default App;
